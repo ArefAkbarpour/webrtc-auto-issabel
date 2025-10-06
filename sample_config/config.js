73acbc92e3744406aa6febebad8ead51b3a8fd1f
ArefAkbarpour <120382242+ArefAkbarpour@users.noreply.github.com>
2025-10-06 16:58:51 +0330

    config.js

---
+++ sample_config/config.js
@@ -0,0 +1,7 @@
+const config = {
+    socketUrl: "wss://webrtc.aaairan.net:8089/ws",
+    sipUri: "sip:1001@webrtc.aaairan.net",
+    sipPassword: "1001@Ashiyaneh",
+    targetUri: "sip:1000@webrtc.aaairan.net",
+    stunServer: "stun:stun.l.google.com:19302"
+};
